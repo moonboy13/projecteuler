4adcee00dd6a189403a2e7147e543682218d836b
Kyle Conlon <conlonkyle@gmail.com>
2021-12-19 21:01:20 -0500

    Updated the unit tests for the extension methods to cover some edge cases. Made a note in the comments on a future refactor to undertake to simplify the testing.

--- C#/ExtentionsTests/NumericExtensionsTests.cs
+++ C#/ExtentionsTests/NumericExtensionsTests.cs
@@ -11,6 +11,10 @@ namespace Extentions.Tests
     [TestClass()]
     public class NumericExtensionsTests
     {
+        /// <summary>
+        /// TODO: Add a data source that can be read in for this. Kinda
+        /// waiting for NUnit to get extended as I prefer working with that.
+        /// </summary>
         [TestMethod()]
         public void FindDivisorsTest()
         {
@@ -22,7 +26,35 @@ namespace Extentions.Tests
 
             HashSet<int> actualDivisors = number.FindDivisors();
 
-            //CollectionAssert.AreEquivalent(expectedDivisors, actualDivisors);
+            Assert.AreEqual(expectedDivisors.Sum(), actualDivisors.Sum());
+        }
+
+        [TestMethod()]
+        public void FindDivisors_SmallNumberTestA()
+        {
+            HashSet<int> expectedDivisors = new HashSet<int>()
+            {
+                1, 2
+            };
+            int number = 4;
+
+            HashSet<int> actualDivisors = number.FindDivisors();
+
+            Assert.AreEqual(expectedDivisors.Sum(), actualDivisors.Sum());
+        }
+
+        [TestMethod()]
+        public void FindDivisors_SmallNumberTestB()
+        {
+            HashSet<int> expectedDivisors = new HashSet<int>()
+            {
+                1
+            };
+            int number = 2;
+
+            HashSet<int> actualDivisors = number.FindDivisors();
+
+            Assert.AreEqual(expectedDivisors.Sum(), actualDivisors.Sum());
         }
     }
 }
