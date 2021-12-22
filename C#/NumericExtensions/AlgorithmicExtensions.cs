5e630ed340d477d4b7eb26f3bf3ac6573dd4784e
Kyle Conlon <conlonkyle@gmail.com>
2021-12-21 23:40:28 -0500

    BigInteger implemenation of problem 25. Would be interesting to test against my own BigInt implementation and compare runtimes. The runtime of this implementation was no more than a second.

---
+++ C#/NumericExtensions/AlgorithmicExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extentions
+{
+    public static class AlgorithmicExtensions
+    {
+        public static int NextFib(this int current, int previous)
+        {
+            if(previous == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return current + previous;
+            }
+        }
+
+        public static long NextFib(this long current, long previous)
+        {
+            if (previous == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return current + previous;
+            }
+        }
+
+        public static BigInteger NextFib(this BigInteger current, BigInteger previous)
+        {
+            if (previous == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return current + previous;
+            }
+        }
+    }
+}
--- C#/Startup/Program.cs
+++ C#/Startup/Program.cs
@@ -1,10 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Solutions;
 
-List<int> res = Problem24.Solve(1000000);
-
-foreach(int i in res)
-{
-    Console.Write(i);
-}
-Console.WriteLine();
+Console.WriteLine(Problem25.Solve(1000));
